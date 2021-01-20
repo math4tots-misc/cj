20bedd4f9a91df41cf468765f6613b5fe4080425
Kyumin Kim <math4tots@users.noreply.github.com>
2021-01-20 13:38:09 -0800

    IO.eprintln

--- src/main/resources/js/cj.IO.js
+++ src/main/resources/js/cj.IO.js
@@ -2,6 +2,9 @@ class MC$cj$IO {
     M$println(TV$T, t) {
         console.log(TV$T.M$toString(t));
     }
+    M$eprintln(TV$T, t) {
+        console.error(TV$T.M$toString(t));
+    }
     M$panic(TV$T, t) {
         throw new Error(TV$T.M$toString(t));
     }
