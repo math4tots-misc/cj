3bb3b23796cbb8ae896b3adb0cc36a57030c0143
Kyumin Kim <math4tots@users.noreply.github.com>
2021-01-28 00:40:38 -0800

    cj.net.Http, sample.net.Download

--- src/main/resources/js/cj.net.Http.js
+++ src/main/resources/js/cj.net.Http.js
@@ -1,2 +1,65 @@
 class MC$cj$net$Http {
+    M$get(url) {
+        if (typeof XMLHttpRequest !== 'undefined') {
+            const request = new XMLHttpRequest();
+            const promise = new Promise((resolve, reject) => {
+                request.onreadystatechange = () => {
+                    if (request.readyState === XMLHttpRequest.DONE) {
+                        // local files, status is 0 upon success in Mozilla Firefox
+                        const status = request.status;
+                        if (status === 0 || (status >= 200 && status < 400)) {
+                            resolve(request.responseText);
+                        } else {
+                            reject(new Error("XMLHttpRequest error: " + status));
+                        }
+                    }
+                };
+            });
+            request.open("GET", url);
+            request.send();
+            return promise;
+        } else if (url.startsWith("https://")) {
+            // nodejs (https)
+            const https = require('https');
+            const promise = new Promise((resolve, reject) => {
+                const request = https.request(url, response => {
+                    let data = '';
+                    response.on('data', chunk => {
+                        data += chunk;
+                    });
+                    response.on('end', () => {
+                        const status = response.statusCode
+                        if (response.complete && status && status >= 200 && status < 400) {
+                            resolve(data);
+                        } else {
+                            reject("https.request error: " + status);
+                        }
+                    });
+                });
+                request.end();
+            });
+            return promise;
+        } else {
+            // nodejs (http)
+            const http = require('http');
+            const promise = new Promise((resolve, reject) => {
+                const request = http.request(url, response => {
+                    let data = '';
+                    response.on('data', chunk => {
+                        data += chunk;
+                    });
+                    response.on('end', () => {
+                        const status = response.statusCode
+                        if (response.complete && status && status >= 200 && status < 400) {
+                            resolve(data);
+                        } else {
+                            reject("http.request error: " + status);
+                        }
+                    });
+                });
+                request.end();
+            });
+            return promise;
+        }
+    }
 }
