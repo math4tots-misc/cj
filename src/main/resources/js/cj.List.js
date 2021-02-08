422d3a260b545c6f120c9318330afdab9bc302c0
Kyumin Kim <math4tots@users.noreply.github.com>
2021-02-08 00:24:16 -0800

    jl1: hello world (sample.JLSample)

--- src/main/resources/js/cj.List.js
+++ src/main/resources/js/cj.List.js
@@ -78,6 +78,9 @@ class MC$cj$List {
     M$filter(self, f) {
         return self.filter(f);
     }
+    M$clone(self) {
+        return Array.from(self);
+    }
     M$toBool(self) {
         return self.length !== 0;
     }
