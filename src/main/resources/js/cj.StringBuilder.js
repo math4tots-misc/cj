ea54993f0f277f098973dcc4fff2c611f316aaed
Kyumin Kim <math4tots@users.noreply.github.com>
2021-02-17 21:34:12 -0800

    cj: CJParser.cj WIP

--- src/main/resources/js/cj.StringBuilder.js
+++ src/main/resources/js/cj.StringBuilder.js
@@ -5,6 +5,12 @@ class MC$cj$StringBuilder {
     M$add(self, string) {
         self.push(string);
     }
+    M$char(self, ch) {
+        self.push(String.fromCodePoint(ch));
+    }
+    M$str(self, str) {
+        self.push(str);
+    }
     M$toString(self) {
         return self.join("");
     }
