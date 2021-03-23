347a9153cb97498338b21394888092e8348b03b2
Kyumin Kim <math4tots@users.noreply.github.com>
2021-03-22 21:12:54 -0700

    notc2: kSTR_LEN, kSTR_ADD, (kARRAY_LEN (WIP))

---
+++ src/main/cj/cjx/n2/prelude.js
@@ -0,0 +1,15 @@
+class Base {
+    toJSON() {
+        const ret = {}
+        for (const prop in this) {
+            if (!prop.startsWith('F')) {
+                throw new Error(`Invalid property name ${prop}`)
+            }
+            ret[prop.slice('F'.length)] = this[prop];
+        }
+        return ret;
+    }
+    toString() {
+        return JSON.stringify(this);
+    }
+}
