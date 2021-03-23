ac5c0d7f7def9c237a07d748b0e34386c9f648cf
Kyumin Kim <math4tots@users.noreply.github.com>
2021-03-23 14:33:36 -0700

    some cleanup (Bool.cj, Char.cj)

--- src/main/resources/js/cj.Char.js
+++ src/main/resources/js/cj.Char.js
@@ -1,19 +1,4 @@
 class MC$cj$Char {
-    M$toInt(self) {
-        return self;
-    }
-    M$toChar(self) {
-        return self;
-    }
-    M$__eq(self, other) {
-        return self === other;
-    }
-    M$hash(self) {
-        return self;
-    }
-    M$toString(self) {
-        return String.fromCodePoint(self);
-    }
     M$repr(self) {
         return "'" + String.fromCodePoint(self).replace(/\n|\r|\t|[\x00-\x1E]|"/g, m => {
             switch (m) {
@@ -33,27 +18,5 @@ class MC$cj$Char {
                     }
             }
         }) + "'";
-    }
-    M$size(self) {
-        return self < 0x10000 ? 1 : 2;
-    }
-
-    M$isDigit(self) {
-        return '0'.codePointAt(0) <= self && self <= '9'.codePointAt(0);
-    }
-    M$isUpper(self) {
-        return 'A'.codePointAt(0) <= self && self <= 'Z'.codePointAt(0);
-    }
-    M$isLower(self) {
-        return 'a'.codePointAt(0) <= self && self <= 'z'.codePointAt(0);
-    }
-    M$isLetter(self) {
-        return this.M$isUpper(self) || this.M$isLower(self);
-    }
-    M$isLetterOrDigit(self) {
-        return this.M$isLetter(self) || this.M$isDigit(self);
-    }
-    M$default() {
-        return 0;
     }
 }
