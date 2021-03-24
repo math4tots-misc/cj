a0a2983dec65c4548f886df47f70da76adc97377
Kyumin Kim <math4tots@users.noreply.github.com>
2021-03-23 21:28:55 -0700

    native inline: more misc

--- src/main/resources/js/cj.Promise.js
+++
@@ -1,6 +0,0 @@
-class MC$cj$Promise {
-    M$done() {}
-    M$then(self, f) { return self.then(f); }
-    M$map(TV$R, self, f) { return self.then(f); }
-    M$flatMap(TV$R, self, f) { return self.then(f); }
-}
--- src/main/resources/js/cj.Time.js
+++
@@ -1,5 +0,0 @@
-class MC$cj$Time {
-    M$now() {
-        return Date.now() / 1000;
-    }
-}
