6bf3169d8709db548f200e64e928dbf5577a34d4
math4tots <math4tots@users.noreply.github.com>
2021-03-28 01:00:21 -0700

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,3 +1,2 @@
 @echo off
-java -cp target/classes crossj.cj.main.JSMain -t -o target/out.js
-node --enable-source-maps target/out.js
+java -cp target/classes crossj.cj.main.JSMain -t -o target/out.js && node --enable-source-maps target/out.js
