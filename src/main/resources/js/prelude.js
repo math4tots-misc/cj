cd2efef40492fa6209134943cc9672da904c32f1
Kyumin Kim <math4tots@users.noreply.github.com>
2021-03-29 00:44:41 -0700

    delete cj.List.js (inline native methods)

--- src/main/resources/js/prelude.js
+++ src/main/resources/js/prelude.js
@@ -85,3 +85,9 @@ function stringToArrayBuffer(string) {
     }
     return buffer;
 }
+
+function* iterFrom(self, start) {
+    for (let i = start; i < self.length; i++) {
+        yield self[i];
+    }
+}
