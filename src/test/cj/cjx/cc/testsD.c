96b08e810708870694816323c456e0093bfa1b2b
Kyumin Kim <math4tots@users.noreply.github.com>
2021-04-01 22:00:36 -0700

    cc: Add a notion of an incomplete struct type

--- src/test/cj/cjx/cc/testsD.c
+++ src/test/cj/cjx/cc/testsD.c
@@ -19,6 +19,14 @@ void test01_incomplete_array_type() {
     aeq(3, ({ int x[2]; x[0]=3; param_decay(x); }));
 }
 
+void test02_incomplete_struct() {
+    aeq(4, ({ struct foo *bar; sizeof(bar); }));
+    aeq(4, ({ struct T *foo; struct T {int x;}; sizeof(struct T); }));
+    aeq(1, ({ struct T { struct T *next; int x; } a; struct T b; b.x=1; a.next=&b; a.next->x; }));
+    aeq(4, ({ typedef struct T T; struct T { int x; }; sizeof(T); }));
+}
+
 int main() {
     test01_incomplete_array_type();
+    test02_incomplete_struct();
 }
