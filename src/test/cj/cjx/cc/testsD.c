7e620531185d8f0a0858375ba0d78c8a097ba3fb
Kyumin Kim <math4tots@users.noreply.github.com>
2021-04-05 09:55:39 -0700

    cc: Add constant expression

--- src/test/cj/cjx/cc/testsD.c
+++ src/test/cj/cjx/cc/testsD.c
@@ -131,6 +131,45 @@ void test07_cond() {
     1 ? -2 : (void)-1;
 }
 
+void test08_constexpr() {
+    aeq(10, ({ enum { ten=1+2+3+4 }; ten; }));
+    aeq(1, ({ int i=0; switch(3) { case 5-2+0*3: i++; } i; }));
+    aeq(8, ({ int x[1+1]; sizeof(x); }));
+    aeq(6, ({ char x[8-2]; sizeof(x); }));
+    aeq(6, ({ char x[2*3]; sizeof(x); }));
+    aeq(3, ({ char x[12/4]; sizeof(x); }));
+    aeq(2, ({ char x[12%10]; sizeof(x); }));
+    aeq(0b100, ({ char x[0b110&0b101]; sizeof(x); }));
+    aeq(0b111, ({ char x[0b110|0b101]; sizeof(x); }));
+    aeq(0b110, ({ char x[0b111^0b001]; sizeof(x); }));
+    aeq(4, ({ char x[1<<2]; sizeof(x); }));
+    aeq(2, ({ char x[4>>1]; sizeof(x); }));
+    aeq(2, ({ char x[(1==1)+1]; sizeof(x); }));
+    aeq(1, ({ char x[(1!=1)+1]; sizeof(x); }));
+    aeq(1, ({ char x[(1<1)+1]; sizeof(x); }));
+    aeq(2, ({ char x[(1<=1)+1]; sizeof(x); }));
+    aeq(2, ({ char x[1?2:3]; sizeof(x); }));
+    aeq(3, ({ char x[0?2:3]; sizeof(x); }));
+    aeq(3, ({ char x[(1,3)]; sizeof(x); }));
+    aeq(2, ({ char x[!0+1]; sizeof(x); }));
+    aeq(1, ({ char x[!1+1]; sizeof(x); }));
+    aeq(2, ({ char x[~-3]; sizeof(x); }));
+    aeq(2, ({ char x[(5||6)+1]; sizeof(x); }));
+    aeq(1, ({ char x[(0||0)+1]; sizeof(x); }));
+    aeq(2, ({ char x[(1&&1)+1]; sizeof(x); }));
+    aeq(1, ({ char x[(1&&0)+1]; sizeof(x); }));
+    aeq(3, ({ char x[(int)3]; sizeof(x); }));
+    aeq(15, ({ char x[(char)0xffffff0f]; sizeof(x); }));
+    aeq(0x10f, ({ char x[(short)0xffff010f]; sizeof(x); }));
+    aeq(4, ({ char x[(int)0xfffffffffff+5]; sizeof(x); }));
+
+    // TODO: Consider whether I want to support pointer arithmetic in
+    // constant expressions.
+    // aeq(8, ({ char x[(int*)0+2]; sizeof(x); }));
+    // aeq(12, ({ char x[(int*)16-1]; sizeof(x); }));
+    // aeq(3, ({ char x[(int*)16-(int*)4]; sizeof(x); }));
+}
+
 int main() {
     test01_incomplete_array_type();
     test02_incomplete_struct();
