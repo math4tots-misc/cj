5e6c0845890d242d5615548d7aa685344fdaeb94
Kyumin Kim <math4tots@users.noreply.github.com>
2021-04-07 09:51:07 -0700

    cc: Add string literal initializer

--- src/test/cj/cjx/cc/testsD.c
+++ src/test/cj/cjx/cc/testsD.c
@@ -187,6 +187,14 @@ void test08_local_var_inits() {
     aeq(2, ({ int x[2][3]={{1,2}}; x[0][1]; }));
     aeq(0, ({ int x[2][3]={{1,2}}; x[1][0]; }));
     aeq(0, ({ int x[2][3]={{1,2}}; x[1][2]; }));
+
+    aeq('a', ({ char x[4]="abc"; x[0]; }));
+    aeq('c', ({ char x[4]="abc"; x[2]; }));
+    aeq(0, ({ char x[4]="abc"; x[3]; }));
+    aeq('a', ({ char x[2][4]={"abc","def"}; x[0][0]; }));
+    aeq(0, ({ char x[2][4]={"abc","def"}; x[0][3]; }));
+    aeq('d', ({ char x[2][4]={"abc","def"}; x[1][0]; }));
+    aeq('f', ({ char x[2][4]={"abc","def"}; x[1][2]; }));
 }
 
 int main() {
